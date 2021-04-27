81334f13891f7d5e6f30e6b6f19c6cadf950bbbb
David Lange <David.Lange@cern.ch>
2021-04-27 10:52:20 +0200

    Create get_requests.py

---
+++ get_requests.py
@@ -0,0 +1,29 @@
+import sys
+sys.path.append('/afs/cern.ch/cms/PPD/PdmV/tools/McM/')
+from rest import McM
+from json import dumps
+
+mcm = McM(dev=True)
+
+# Example to get  ALL requests which are member of a given campaign and are submitted
+# It uses a generic search for specified columns: query='status=submitted'
+# Queries can be combined: query='status=submitted&member_of_campaign=Summer12'
+
+campaigns= ["RunIISummer20UL18wmLHEGEN","RunIISummer20UL18pLHEGEN","RunIISummer20UL18GEN"]
+only_new=False
+
+for campaign in campaigns:
+    if only_new:
+       f=open(campaign+"_new.txt",'w')
+       campaign_requests = mcm.get('requests', query='member_of_campaign='+campaign+'&status=new')
+    else:
+       f=open(campaign+".txt",'w')
+       campaign_requests = mcm.get('requests', query='member_of_campaign='+campaign)
+       
+    for request in campaign_requests:
+        f.write(request['prepid']+"\n")
+
+    cmsdrivers = mcm.get('campaigns', campaign, method='get_cmsDrivers')
+    f.write('Single request "%s":\n%s' % (campaign, dumps(cmsdrivers, indent=4)))
+    f.write('\n')
+    f.close()
